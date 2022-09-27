32919735bea5b747c74ccb5bdb50bfc222deef93
Moldoteck <cpadureac@gmail.com>
2022-09-27 22:26:10 +0300

    first steps magic login

--- src/components/MainBlock.tsx
+++ src/components/MainBlock.tsx
@@ -9,10 +9,12 @@ import classnames, {
   alignItems,
   display,
   flexDirection,
+  fontFamily,
   height,
   justifyContent,
   padding,
   space,
+  textColor,
   width,
 } from 'classnames/tailwind'
 import OptionButton from './OptionButton'
@@ -52,14 +54,109 @@ export default function () {
   //for each slot- empty or with document, can replace, regenerate? inplace
   //so we need a slotblock component, a button to upgrade plan
   //also we need a profile page, where user can change email(migration of docs), can upgrade/downgrade plan and can delete account
-  
+
   //on front page: instruction in Three steps: 1. upload pdf 2. get qrcode 3. print your qrcode and put it everywhere
   return (
     <div className={container}>
       <HeaderText>Get QR codes</HeaderText>
       <HeaderText>for your files</HeaderText>
       <div className={imageContainer}>
-        <img src="https://i.ibb.co/TbrQ5L5/qrsample.png"></img>
+        <div
+          className={classnames(
+            display('flex'),
+            flexDirection('flex-row'),
+            justifyContent('justify-center'),
+            alignItems('items-center'),
+            space('space-x-2')
+          )}
+        >
+          <div
+            className={classnames(
+              display('flex'),
+              flexDirection('flex-col'),
+              justifyContent('justify-center'),
+              alignItems('items-center'),
+              space('space-x-2')
+            )}
+          >
+            <img
+              src="https://i.ibb.co/q1dCB8x/icons8-pdf-64.png"
+              className={width('w-32')}
+            />
+            <h3
+              className={classnames(
+                fontFamily('font-mono'),
+                textColor('text-white')
+              )}
+            >
+              Upload your <br />
+              documents
+            </h3>
+          </div>
+
+          <h3
+            className={classnames(
+              fontFamily('font-mono'),
+              textColor('text-white')
+            )}
+          >
+            ----------&gt;
+          </h3>
+          <div
+            className={classnames(
+              display('flex'),
+              flexDirection('flex-col'),
+              justifyContent('justify-center'),
+              alignItems('items-center'),
+              space('space-x-2')
+            )}
+          >
+            <img
+              src="https://i.ibb.co/TbrQ5L5/qrsample.png"
+              className={width('w-32')}
+            />
+            <h3
+              className={classnames(
+                fontFamily('font-mono'),
+                textColor('text-white')
+              )}
+            >
+              Get QR code
+            </h3>
+          </div>
+
+          <h3
+            className={classnames(
+              fontFamily('font-mono'),
+              textColor('text-white')
+            )}
+          >
+            ----------&gt;
+          </h3>
+          <div
+            className={classnames(
+              display('flex'),
+              flexDirection('flex-col'),
+              justifyContent('justify-center'),
+              alignItems('items-center'),
+              space('space-x-2')
+            )}
+          >
+            <img
+              src="https://i.ibb.co/cypf4zb/icons8-print-100.png"
+              className={width('w-32')}
+            />
+            <h3
+              className={classnames(
+                fontFamily('font-mono'),
+                textColor('text-white')
+              )}
+            >
+              Print it <br />
+              anywhere
+            </h3>
+          </div>
+        </div>
         <div className={buttonContainer}>
           <OptionButton
             title="Test it"
